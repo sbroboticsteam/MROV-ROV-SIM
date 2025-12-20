53f8dbee22d70e24e86c633fa19d6537fc24d787
odinroast <ruthvick2005@gmail.com>
2025-12-20 10:11:21 -0500

    1. Edited display.launch.py to read urdf instead of xacro, 2. Inside the urdf folder, edited revolute joints to become ball joints, converted xacro to a urdf and then the urdf to an sdf, and changed all system relative paths to package relative paths

--- src/teleoparm_description/launch/display.launch.py
+++ src/teleoparm_description/launch/display.launch.py
@@ -11,7 +11,7 @@ from ament_index_python.packages import get_package_share_directory
 def generate_launch_description():
     share_dir = get_package_share_directory('teleoparm_description')
 
-    xacro_file = os.path.join(share_dir, 'urdf', 'teleoparm.xacro')
+    xacro_file = os.path.join(share_dir, 'urdf', 'teleoparm.urdf')
     robot_description_config = xacro.process_file(xacro_file)
     robot_urdf = robot_description_config.toxml()
 
---
+++ src/teleoparm_description/urdf/xacro2sdf.md
@@ -0,0 +1,2 @@
+ros2 run xacro xacro -o model.urdf model.xacro
+gz sdf -p model.urdf > model.sdf
