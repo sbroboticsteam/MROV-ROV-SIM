4c462989cff63ed6dedf810316bb788c261c5260
odinroast <ruthvick2005@gmail.com>
2025-12-27 10:49:15 -0500

    Upgraded fake imu publisher node to include an action server that uses SLERP to rotate the quaternions over a duration, this data is automatically published back into the imu topic which will then be used by the controller node to manipulate the arm, edited package.xml to include new dependencies

--- src/teleoparm_description/teleoparm_description/imu_fake_pub.py
+++ src/teleoparm_description/teleoparm_description/imu_fake_pub.py
@@ -1,9 +1,17 @@
 import rclpy
-import sys
 from rclpy.node import Node
 from sensor_msgs.msg import Imu
 from geometry_msgs.msg import Quaternion, Vector3
 from std_msgs.msg import Header
+# Action server imports
+from rclpy.action import ActionServer
+from imu_action_interfaces.action import ImuGoal
+import time
+# Dependencies to perform SLERP
+import numpy as np
+from scipy.spatial.transform import Rotation as R, Slerp
+# Single threaded executor for async functions
+from rclpy.executors import MultiThreadedExecutor
 
 # Quaternion Structure
 # Roll (Y) -> (90, -90), Pitch (X) (-90, 90), Yaw:(+90, -90), W (0)
@@ -43,6 +51,15 @@ class IMUFakePublisher(Node):
         self.data.angular_velocity_covariance = [-1.0] * 9
         self.data.linear_acceleration_covariance = [-1.0] * 9
 
+        # Create a ROS2 Action server
+        self.action_server = ActionServer(
+            self, 
+            ImuGoal, # Type IMU Goal
+            f'imu_goal_{self.imu_name}', 
+            self.action_callback # Callback function for the action server
+        )
+
+    # Define the timer callback function
     def timer_callback(self):
         # Stamp the data with the current time and publish
         self.data.header.stamp = self.get_clock().now().to_msg()
@@ -53,18 +70,123 @@ class IMUFakePublisher(Node):
         # Log the publishing
         self.get_logger().info("Published NULL Fake IMU Data")
 
+    # Define the action callback function 
+    def action_callback(self, goal_handle):
+        try:
+            # Debug output
+            self.get_logger().info('Executing goal...')
+            # Init the feedback message
+            feedback_msg = ImuGoal.Feedback()
+            feedback_msg.progress = 0.0
+            feedback_msg.current_orientation = self.data.orientation
+
+            # Setup SLERP
+            r_start = R.from_quat(self.quat_to_array(self.data.orientation))
+            r_end = R.from_quat(self.quat_to_array(goal_handle.request.goal_orientation))
+            # Key times and Rotations (Time range)
+            key_times = [0.0, goal_handle.request.duration]
+            key_rots = R.concatenate([r_start, r_end])
+            # Perform slerp over these times
+            slerp = Slerp(key_times, key_rots)
+
+            # Start time and feedback publishing rate
+            start_time = self.get_clock().now()
+
+            # While node is not blocked
+            while rclpy.ok():
+                # Get the time that has elapsed
+                elapsed = (self.get_clock().now() - start_time).nanoseconds/1e9
+                # Clamp the time that has passed to duration
+                elapsed = min(elapsed, goal_handle.request.duration)
+
+                # Query the slerp function at this particular time
+                r_current = slerp(elapsed)
+                quat_current = r_current.as_quat()
+
+                # Update the IMU Data for the publisher
+                self.data.orientation = self.array_to_quat(quat_current)
+
+                # Handle Action Cancellation
+                # CHECK HERE - before or after updating orientation
+                if goal_handle.is_cancel_requested:
+                    # Handle cancellation
+                    goal_handle.canceled()  # Mark as canceled
+                    
+                    result = ImuGoal.Result()
+                    result.reached = False
+                    result.message = "Goal was canceled by user"
+                    result.final_orientation = self.data.orientation  # Current position
+                    
+                    return result  # Exit early
+
+
+                # Calculate the progress to send to feedback
+                progress = elapsed / goal_handle.request.duration
+
+                # Send the feedback message
+                feedback_msg.progress = progress
+                feedback_msg.current_orientation = self.data.orientation
+                goal_handle.publish_feedback(feedback_msg)
+                # Break out of the loop if elapsed > duration
+                if elapsed >= goal_handle.request.duration:
+                    break
+                # In loop to delay publishing 
+                time.sleep(0.02)
+            
+            # Create result data and return it
+            goal_handle.succeed()
+            result = ImuGoal.Result()
+            result.reached = True
+            result.message = f"Goal {goal_handle.request.goal_orientation} for {self.imu_name} successfully reached..."
+            result.final_orientation = self.data.orientation
+
+            # Return the result
+            return result
+        
+        # Catch the error
+        except Exception as e:
+            self.get_logger().error(f"Action failed with exception: {e}")
+            import traceback
+            self.get_logger().error(traceback.format_exc())
+            
+            goal_handle.abort()
+            result = ImuGoal.Result()
+            result.reached = False
+            result.message = f"Error: {str(e)}"
+            result.final_orientation = self.data.orientation
+            return result
+
+
+    # Function to convert from geometry_msgs/Quaternion to numpy array
+    def quat_to_array(self, q):
+        return np.array([q.x, q.y, q.z, q.w], dtype=float)
+    
+    # Fuction to convert np list to geometry_msgs/Quaternion
+    def array_to_quat(self, arr):
+        q = Quaternion()
+        q.x, q.y, q.z, q.w = map(float, arr)
+        return q
+
+    
+
 def main(args=None):
     # Init ROS2
     rclpy.init(args=args)
         
     # Make an object for the fake imu data publisher node
     imu_fake_publisher = IMUFakePublisher()
-    # Spin that node
-    rclpy.spin(imu_fake_publisher)
+    
+    # Create a SingleThreaded executor
+    executor = MultiThreadedExecutor()
+    executor.add_node(imu_fake_publisher)
 
-    # Cleanup
-    imu_fake_publisher.destroy_node()
-    rclpy.shutdown()
+    try:
+        # Spin that node
+        executor.spin()
+    finally:
+            executor.shutdown()
+            imu_fake_publisher.destroy_node()
+            rclpy.shutdown()
         
 if __name__ == "__main__":
     main()
